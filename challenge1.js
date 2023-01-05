ac84499fe8020de4764f4683d46d8c218db8f308
Varun Pandey <varpa20@gmail.com>
2023-01-05 14:19:26 +0530

    all js files

---
+++ challenge1.js
@@ -0,0 +1,15 @@
+console.log(10+"20");   // 
+
+console.log(9-"5");   // bug 
+
+console.log("java" +"script");
+
+console.log("java " +"script");
+
+console.log("" +"");
+
+console.log("varun" - "pandey");   // NaN
+
+console.log( false - true);    // -1
+
+
---
+++ notanumber.js
@@ -0,0 +1,18 @@
+var phonenumber = 7355097315;
+var myName = "varun";
+
+
+console.log(isNaN(phonenumber));
+console.log(isNaN(myName));
+
+
+if (isNaN(myName)){
+    console.log("Please enter valid number");
+}
+
+var players;
+console.log(typeof(players));
+
+var myhome = null;
+console.log(myhome);
+console.log(typeof(myhome));
